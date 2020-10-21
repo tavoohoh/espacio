538481d43c771113f6412a57f9e17ed99e301a82
Gustavo Santamaria <gustavo.santamaria@rappi.com>
2020-10-21 18:43:01 -0500

    added firebase and ngx translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'espacio';
+export class AppComponent implements OnInit {
+  constructor(
+    private translate: TranslateService
+  ) { }
+
+  ngOnInit() {
+    let userLanguage = 'es';
+
+    if (window.localStorage.getItem('userLanguage')) {
+      userLanguage = window.localStorage.getItem('userLanguage');
+    } else {
+      window.localStorage.setItem('userLanguage', userLanguage);
+    }
+
+    this.translate.setDefaultLang(userLanguage);
+    this.translate.use(userLanguage);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+// https://github.com/ngx-translate/core
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// https://github.com/angular/angularfire
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { firebaseConfig } from '../espacio';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +29,10 @@ import { OrderComponent } from './components/ui/order/order.component';
 import { OrderDetailComponent } from './components/ui/order-detail/order-detail.component';
 import { ModalComponent } from './components/ui/modal/modal.component';
 import { LoaderComponent } from './components/ui/loader/loader.component';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 const COMPONENTS = [
   HeaderComponent,
@@ -54,7 +68,17 @@ const FIELD_COMPONENTS = [
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireStorageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
