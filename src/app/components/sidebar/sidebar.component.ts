6855548dd4896ce41d9c943a50e3f8458c02be9f
Gustavo Santamaria <gustavo.santamaria@rappi.com>
2020-10-23 18:20:24 -0500

    added logo, favicon, site metadata and some styles to the sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.sass']
 })
 export class SidebarComponent implements OnInit {
+  public readonly appVersion = environment.version;
 
   constructor() { }
 
